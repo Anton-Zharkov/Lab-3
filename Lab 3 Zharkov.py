54cc08dee2e3f506e81535f179723d6fce54e33b
Anton-Zharkov <anton.zharkoffkz@gmail.com>
2025-02-15 18:28:39 +0300

    Add files via upload

---
+++ Lab 3 Zharkov.py
@@ -0,0 +1,68 @@
+class Book:
+    def __init__(self, name, author):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(name='{self.name}', author='{self.author}')"
+
+
+class PaperBook(Book):
+    def __init__(self, name, author, pages):
+        super().__init__(name, author)
+        self.pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, value):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Количество страниц должно быть положительным целым числом.")
+        self._pages = value
+
+    def __str__(self):
+        return f"PaperBook(name='{self.name}', author='{self.author}', pages={self.pages})"
+
+
+class AudioBook(Book):
+    def __init__(self, name, author, duration):
+        super().__init__(name, author)
+        self.duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, value):
+        if not isinstance(value, (float, int)) or value <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом.")
+        self._duration = float(value)
+
+    def __str__(self):
+        return f"AudioBook(name='{self.name}', author='{self.author}', duration={self.duration})"
+
+
+# Примеры использования
+try:
+    
+    another_paper_book = PaperBook("Мастер и Маргарита", "Михаил Булгаков", 448)
+    another_audio_book = AudioBook("Мастер и Маргарита", "Михаил Булгаков", 12.0)
+
+    print(another_paper_book)
+    print(another_audio_book)
+
+    # Попробуем установить некорректные значения
+    another_paper_book.pages = -100  # Это вызовет ValueError
+except ValueError as e:
+    print(e)
